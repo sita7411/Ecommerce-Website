78f8e394ca95bbb3032a9e785a5cb1effc551ebc
sita7411 <sitausdadiya7411@gmail.com>
2025-11-10 13:59:12 +0530

    Update ReturnPage.jsx

--- frontend/src/Pages/ReturnPage.jsx
+++ frontend/src/Pages/ReturnPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../Pages/CSS/ReturnPage.css";
-import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 
 const ReturnPage = () => {
